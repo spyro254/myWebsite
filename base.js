3fdbeb06877e12d53da73aaad812b470a8ba5030
spyro254 <ali254pro@gmail.com>
2018-08-19 19:32:00 +0100

    burger menu re-written clean - all code present - needs tidying

--- base.js
+++ base.js
@@ -10,11 +10,27 @@ const XnavOnHover = function() {
     }
 }
 
-const navOnHover = function() {
-    let burgerLines = document.getElementById('burger-lines');
-    burgerLines.classList.add('burger--open')
+const burgerOpen = function(burgerId) {
+    let burger = document.getElementById(burgerId);
+    burger.classList.add('burger-line--open');
+    moveBurgerItem('burgerContainer');
+}
+
+const burgerClose = function(burgerId) {
+    let burger = document.getElementById(burgerId);
+    burger.classList.remove('burger-line--open')
+}
+
+const moveBurgerItem = function(burgerId) {
+    let burger = document.getElementById(burgerId);
+    let burgerItemArray = Array.from(burger.getElementsByClassName('burger-item'))
+    for (each of burgerItemArray) {
+        each.classList.add('burger-item--show');
+    }
 }
 
 window.onload = function() {
-    document.getElementById('test-burger').addEventListener('mouseover', navOnHover)
+    burger = document.getElementById('burgerContainer');
+    burger.addEventListener('mouseover', function(){burgerOpen('burgerLine')});
+    burger.addEventListener('mouseout', function(){burgerClose('burgerLine')});
 }
